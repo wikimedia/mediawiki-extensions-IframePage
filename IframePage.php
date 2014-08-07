d0b75fcd353e70bf3042f28e33eabafbaefaa673
Ike Hecht <tosfos@yahoo.com>
2014-08-06 19:04:39 -0700

    fix i18n, change global names
    Change-Id: Iffa9306c93e3beddc147759f280d27ccb5c10b14

--- IframePage.php
+++ IframePage.php
@@ -8,7 +8,7 @@ EOT;
 	exit( 1 );
 }
 
-$wgExtensionCredits[ 'specialpage' ][] = array(
+$wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'IframePage',
 	'author' => 'Ike Hecht for [//www.wikiworks.com WikiWorks]',
@@ -17,16 +17,29 @@ $wgExtensionCredits[ 'specialpage' ][] = array(
 	'version' => '0.1',
 );
 
-# If set to true, users can set path= in the call to the special page. This will allow adding to the iframe path set by $wgifSrc in LocalSettings.
-# There is a slight security risk in this in case the iframe source's server was compromised or something, so default to false.
-# (Maybe there's an iframe "farm" out there where only some scripts are trustworthy.)
-$wgifAllowPath = false;
+/**
+ * An associative array containing the iframe src. Mandatory.
+ * The key in this array is expected to be a simple string that will be used as the title of a
+ * subpage of Special:IframePage. The value should contain the actual iframe src URL.
+ * The key can be omitted. That is because navigating to Special:Iframepage will simply pull the
+ * URL contained in the first value in the array.
+ */
+$wgIframePageSrc = array();
 
-$wgAutoloadClasses[ 'SpecialIframePage' ] = __DIR__ . '/SpecialIframePage.php';
-$wgExtensionMessagesFiles[ 'IframePage' ] = __DIR__ . '/IframePage.i18n.php';
-$wgExtensionMessagesFiles[ 'IframePageAlias' ] = __DIR__ . '/IframePage.alias.php';
-$wgMessagesDirs[ 'IframePage' ] = __DIR__ . '/i18n';
-$wgSpecialPages[ 'IframePage' ] = 'SpecialIframePage';
+/**
+ * If set to true, users can set path= in the call to the special page.
+ * This will allow adding to the iframe path set by $wgIframePageSrc in LocalSettings.
+ * There is a slight security risk in this in case the iframe source's server was compromised
+ * or something, so default to false.
+ * (Maybe there's an iframe "farm" out there where only some scripts are trustworthy.)
+ */
+$wgIframePageAllowPath = false;
+
+$wgAutoloadClasses['SpecialIframePage'] = __DIR__ . '/SpecialIframePage.php';
+$wgExtensionMessagesFiles['IframePage'] = __DIR__ . '/IframePage.i18n.php';
+$wgExtensionMessagesFiles['IframePageAlias'] = __DIR__ . '/IframePage.alias.php';
+$wgMessagesDirs['IframePage'] = __DIR__ . '/i18n';
+$wgSpecialPages['IframePage'] = 'SpecialIframePage';
 
 $wgResourceModules['ext.IframePage'] = array(
 	'styles' => 'IframePage.css',
--- SpecialIframePage.php
+++ SpecialIframePage.php
@@ -1,5 +1,7 @@
 <?php
+
 class SpecialIframePage extends SpecialPage {
+
 	function __construct() {
 		parent::__construct( 'IframePage' );
 	}
@@ -7,7 +9,7 @@ class SpecialIframePage extends SpecialPage {
 	//$srcName is expected to be a key in the $wgifSrc array. It is set by navigating to a subpage of this special page.
 	//If set, it will be used as the Page Title.
 	function execute( $srcName ) {
-		global $wgifSrc, $wgifAllowPath;
+		global $wgIframePageSrc, $wgIframePageAllowPath;
 		$request = $this->getRequest();
 		$output = $this->getOutput();
 		$output->addModules( 'ext.IframePage' );
@@ -15,34 +17,46 @@ class SpecialIframePage extends SpecialPage {
 
 		$path = $request->getVal( 'path' );
 
-		if ( !isset( $wgifSrc ) ) {
+		if ( !isset( $wgIframePageSrc ) ) {
 			throw new ErrorPageError( 'errorpagetitle', 'iframepage-error-wgifsrc' );
 		}
 
-		$srcName = $name = str_replace('_', ' ', urldecode( $srcName ) );
+		$srcName = $name = str_replace( '_', ' ', urldecode( $srcName ) );
 
-		if ( isset( $wgifSrc[$srcName] ) ) {
-			$src = $wgifSrc[$srcName];
+		if ( isset( $wgIframePageSrc[$srcName] ) ) {
+			$src = $wgIframePageSrc[$srcName];
 		} else {
 			//Default to first key/value pair of array
-			$srcName = key( $wgifSrc );
-			$src = reset( $wgifSrc );
+			$srcName = key( $wgIframePageSrc );
+			$src = reset( $wgIframePageSrc );
 		}
 
-		if ( $wgifAllowPath && isset( $path ) ) {
+		if ( $wgIframePageAllowPath && isset( $path ) ) {
 			$src .= $path;
 		}
 
 		//User can set $wgifSrc without a key. If so, we don't want the page title to be '0'
 		if ( $srcName != '0' ) {
 			//Do users always want this to be the page title?
-			$output->setPageTitle( $srcName ); /*@todo This should really be an i18n msg key. But that's not so simple.*/
+			/** @todo This should really be an i18n msg key. But that's not so simple. */
+			$output->setPageTitle( $srcName );
 		}
 
 		$html = Html::openElement( 'div', array( 'id' => 'iframeframe' ) );
-		$html .= Html::element( 'iframe', array( 'id' => "iframe-$srcName", 'src' => $src, 'frameborder' => '0', 'width' => '100%', 'height' => '100%' ) );
+		$html .= Html::element( 'iframe',
+				array( 'id' => "iframe-$srcName",
+				'src' => $src,
+				'frameborder' => '0',
+				'width' => '100%',
+				'height' => '100%'
+				)
+		);
 		$html .= Html::closeElement( 'div' );
 
 		$output->addHTML( $html );
+	}
+
+	protected function getGroupName() {
+		return 'other';
 	}
 }
