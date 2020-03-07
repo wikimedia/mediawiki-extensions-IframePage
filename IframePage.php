e781b826db6624c8a62c97a7986897f6de7bb0f0
zoranzoki21 <zorandori4444@gmail.com>
2020-03-07 14:52:41 +0100

    Convert IframePage to use extension registration
    Bug: T243629
    Change-Id: I494f55c2e99daeeaf45362049e127239dac2e23c

--- IframePage.php
+++ IframePage.php
@@ -1,48 +1,15 @@
 <?php
-# Alert the user that this is not a valid access point to MediaWiki
-# if they try to access the special pages file directly.
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo <<<EOT
-To install this extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/IframePage/IframePage.php" );
-EOT;
-	exit( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'IframePage' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['IframePage'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['IframePageAlias'] = __DIR__ . '/IframePage.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for the IframePage extension. ' .
+		'Please use wfLoadExtension() instead, ' .
+		'see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the IframePage extension requires MediaWiki 1.29+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path' => __FILE__,
-	'name' => 'IframePage',
-	'author' => 'Ike Hecht for [//www.wikiworks.com WikiWorks]',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:IframePage',
-	'descriptionmsg' => 'iframepage-desc',
-	'version' => '0.2.1',
-);
-
-/**
- * An associative array containing the iframe src. Mandatory.
- * The key in this array is expected to be a simple string that will be used as the title of a
- * subpage of Special:IframePage. The value should contain the actual iframe src URL.
- * The key can be omitted. That is because navigating to Special:Iframepage will simply pull the
- * URL contained in the first value in the array.
- */
-$wgIframePageSrc = array();
-
-/**
- * If set to true, users can set path= in the call to the special page.
- * This will allow adding to the iframe path set by $wgIframePageSrc in LocalSettings.
- * There is a slight security risk in this in case the iframe source's server was compromised
- * or something, so default to false.
- * (Maybe there's an iframe "farm" out there where only some scripts are trustworthy.)
- */
-$wgIframePageAllowPath = false;
-
-$wgAutoloadClasses['SpecialIframePage'] = __DIR__ . '/SpecialIframePage.php';
-$wgExtensionMessagesFiles['IframePageAlias'] = __DIR__ . '/IframePage.alias.php';
-$wgMessagesDirs['IframePage'] = __DIR__ . '/i18n';
-$wgSpecialPages['IframePage'] = 'SpecialIframePage';
-
-$wgResourceModules['ext.IframePage'] = array(
-	'styles' => 'IframePage.css',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'IframePage',
-);
